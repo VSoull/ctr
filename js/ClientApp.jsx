c7c0ca8145be273658b98706e695b83da25fdcbf
LEVI9\v.hetriuc <v.hetriuc@levi9.com>
2018-07-26 13:32:26 +0300

    finished app component + added eslint pre

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const MyTitle = function MyTitle(props) {
-  return React.createElement('div', { style: { color: props.color } }, props.title);
-};
-const MyFirstComponent = function MyFirstComponent() {
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      React.createElement(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-      React.createElement(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-      React.createElement(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-      React.createElement(MyTitle, { title: 'House of Cards', color: 'peru' })
-    )
-  );
-};
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>video app</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
+    </div>
+  </div>
+);
 
-render(React.createElement(MyFirstComponent), document.getElementById('app'));
+render(<App />, document.getElementById('app'));
